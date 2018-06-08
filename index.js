4a592a3c4b5cb3ba576bd399fc82ed2bef63467a
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-08 11:12:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const dateformat = require('dateformat');
-const datediff = require('date-diff')
 
 
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({disableEveryone: false});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
